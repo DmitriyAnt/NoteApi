ee3e67b9993fba0e6c40b4c146818ecb87184bad
Dmitriy <d_antonov@list.ru>
2022-04-04 21:03:54 +0300

    update fixture loader

--- load_fixture.py
+++ load_fixture.py
@@ -1,4 +1,6 @@
 import json
+
+import click
 from sqlalchemy.exc import IntegrityError
 from api import db
 from api.models.note import NoteModel
@@ -6,27 +8,32 @@ from api.models.user import UserModel
 from config import BASE_DIR
 
 # path_to_fixture = os.path.join(base_dir, "fixtures" , "users.json")
-path_to_fixture = BASE_DIR / "fixtures" / "notes.json"
+# path_to_fixture = BASE_DIR / "fixtures" / "notes.json"
 models = {
     "NoteModel": NoteModel,
     "UserModel": UserModel,
 }
 
-with open(path_to_fixture, "r", encoding="UTF-8") as f:
-    data = json.load(f)
-    count = 0
-    model_name = data["model"]
-    model = models[model_name]
-    for record in data["records"]:
-        model_obj = model(**record)
-        db.session.add(model_obj)
-        try:
-            db.session.commit()
-            count += 1
-        except IntegrityError:
-            db.session.rollback()
 
-    print(f"{count} records created")
+@click.command
+@click.argument('message')
+def load_fixtures(message):
+    path_to_fixture = BASE_DIR / "fixtures" / message
+    with open(path_to_fixture, "r", encoding="UTF-8") as f:
+        data = json.load(f)
+        count = 0
+        model_name = data["model"]
+        model = models[model_name]
+        for record in data["records"]:
+            model_obj = model(**record)
+            db.session.add(model_obj)
+            try:
+                db.session.commit()
+                count += 1
+            except IntegrityError:
+                db.session.rollback()
+
+        print(f"{count} records created")
 
 
 # users_data = UserRequestSchema(many=True).loads(f.read())
@@ -40,3 +47,7 @@ with open(path_to_fixture, "r", encoding="UTF-8") as f:
 #     except IntegrityError:
 #         db.session.rollback()
 #         print(f"User {user.username} already exists")
+
+
+if __name__ == "__main__":
+    load_fixtures()
