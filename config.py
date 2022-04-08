1e3542fa72082d8eb21700900e86f245db2889f9
Dmitriy <d_antonov@list.ru>
2022-04-08 20:27:56 +0300

    change config

--- config.py
+++ config.py
@@ -15,8 +15,8 @@ security_definitions = {
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR / "base.db"}')\
-         .replace("://", "ql://", 1)  # only for Heroku
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR / "base.db"}')#\
+         # .replace("://", "ql://", 1)  # only for Heroku
     TEST_DATABASE_URI = f'sqlite:///{BASE_DIR / "base.db"}'  # 'sqlite:///' + os.path.join(base_dir, 'test.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Зачем эта настройка: https://flask-sqlalchemy-russian.readthedocs.io/ru/latest/config.html#id2
     DEBUG = True
