f4bf0a153d3e5bd76ee759dc418887a0c342ac98
Dmitriy <myjashik@bk.ru>
2022-04-15 18:49:42 +0300

    fix all tests

--- helpers/shortcuts.py
+++ helpers/shortcuts.py
@@ -5,5 +5,5 @@ from flask_babel import _
 def get_object_or_404(model, object_id):
     obj = model.query.get(object_id)
     if not obj:
-        raise NotFound(description=_("{model} with id=%(object_id)s not found", bject_id=object_id))
+        raise NotFound(description=_("Model with id=%(object_id)s not found", object_id=object_id))
     return obj
